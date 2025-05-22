b5865f986e4172acd864b6b4481d2c5d1b826842
YumetoFukuura <y.fukuura@wander-lust.io>
2025-05-22 22:11:19 +0900

    Initial commit

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ async def remove_background(file: UploadFile = File(...)):
     input_bytes = await file.read()
     output_bytes = remove(input_bytes)
     return StreamingResponse(io.BytesIO(output_bytes), media_type="image/png")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # ← ここでPORT環境変数を見る
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
